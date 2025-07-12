51ee1bbac09a9a38e0345e0295857a887f2a4b81
IMayHeMI <oleinik1999@gmail.com>
2025-07-12 13:40:30 -0500

    Add final logic (darkening effect), final bagfix.

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const container = document.querySelector(".container");
 const sizeButton = document.querySelector("#sizeButton")
+const resetButton = document.querySelector("#resetButton")
 const cellSize = 30;
 
-let size = 16 * 16;
+let size = 16;
+let isMouseButtonHold = false;
 
-Array.from({length : size}).forEach(() => {
+window.addEventListener("mousedown", () => isMouseButtonHold = true)
+window.addEventListener("mouseup", () => isMouseButtonHold = false)
+
+Array.from({length : size * size}).forEach(() => {
     const div = document.createElement("div");
     div.classList.add("cell");
+    div.setAttribute('draggable', 'false');
     container.appendChild(div);           
+    div.dataset.alpha = 0;
     div.addEventListener("mouseover", event => {
-        event.target.style.backgroundColor = 
-        `rgb(${Math.floor(Math.random() * 255) + 1},
-        ${Math.floor(Math.random() * 255) + 1},
-        ${Math.floor(Math.random() * 255) + 1})`;       
-    })
+        if(isMouseButtonHold) darkening(event.target);
+
+        // event.target.style.backgroundColor = 
+        // `rgb(${Math.floor(Math.random() * 255) + 1},
+        // ${Math.floor(Math.random() * 255) + 1},
+        // ${Math.floor(Math.random() * 255) + 1})`;                            
+    });
+    div.addEventListener('mousedown', event => {
+        if(isMouseButtonHold) darkening(event.target);
+    });
 })
 
 sizeButton.addEventListener("click", () => {
@@ -23,7 +35,7 @@ sizeButton.addEventListener("click", () => {
         resize();
     } 
     else
-        alert("Please, use the correct value")   
+        alert("Please, use the correct value from 8 to 64")   
 });
 
 function resize(){ 
@@ -34,9 +46,25 @@ function resize(){
     Array.from({length : size * size}).forEach(() => {
     const div = document.createElement("div");
     div.classList.add("cell");
+    div.setAttribute('draggable', 'false');
     container.appendChild(div);
     div.addEventListener("mouseover", event => {
-        event.target.style.backgroundColor = "wheat";       
+        if(isMouseButtonHold) darkening(event.target);              
+    });
+    div.addEventListener('mousedown', event => {
+         darkening(event.target)
     })
 })
 }
+
+function darkening(cell){
+    let alpha = parseFloat(cell.dataset.alpha) || 0;
+    if (alpha < 1){
+        alpha += 0.1;
+        if (alpha > 1) alpha = 1;
+        cell.style.backgroundColor = `rgba(0, 0, 0, ${alpha})`;
+        cell.dataset.alpha = alpha;
+    }
+}
+
+resetButton.addEventListener("click", resize);
